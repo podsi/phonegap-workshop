d0303f21280d56748d6d395808e58c3f46c3ee34
podsi <maxmilestone@gmail.com>
2014-08-16 15:44:39 +0200

    creating a view class

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
 var app = {
 
-  findByName: function() {
-    var self = this;
-
-    this.store.findByName( $( '.search-key' ).val( ), function( employees ) {
-      $( '.employee-list' ).html( self.employeeLiTpl( employees ) );
-    } );
-  },
-
   initialize: function() {
     var self = this;
 
@@ -19,14 +11,9 @@ var app = {
     // this.store = new WebSqlStore();
 
     this.store = new WebSqlStore( function( ) {
-      self.renderHomeView( );
+      $( 'body' ).html( new HomeView( self.store ).render( ).el );
     } );
 
-    $( '.search-key' ).on( 'keyup', $.proxy( this.findByName, this ) );
-  },
-
-  renderHomeView: function( ) {
-    $( 'body' ).html( this.homeTpl );
     $( '.search-key' ).on( 'keyup', $.proxy( this.findByName, this ) );
   },
 
