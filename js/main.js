174f7d5581994a8c848138236e468484c28ed693
podsi <maxmilestone@gmail.com>
2014-08-16 11:57:52 +0200

    set up a single page application

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 var app = {
 
   findByName: function() {
-    console.log('findByName');
-    this.store.findByName($('.search-key').val(), function(employees) {
+    console.log( 'findByName' );
+    this.store.findByName( $( '.search-key' ).val( ), function( employees ) {
         var l = employees.length;
         var e;
-        $('.employee-list').empty();
-        for (var i=0; i<l; i++) {
-            e = employees[i];
-            $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+        $( '.employee-list' ).empty( );
+        for( var i = 0; i < l; i++ ) {
+          e = employees[ i ];
+          $( '.employee-list' ).append( '<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>' );
         }
-    });
+    } );
   },
 
   initialize: function() {
@@ -20,21 +20,32 @@ var app = {
     // this.store = new LocalStorageStore();
     // this.store = new WebSqlStore();
 
-    this.store = new WebSqlStore(function() {
-      self.showAlert('Store Initialized', 'Info');
-    });
+    this.store = new WebSqlStore( function( ) {
+      self.renderHomeView( );
+    } );
 
-    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    $( '.search-key' ).on( 'keyup', $.proxy( this.findByName, this ) );
   },
 
-  showAlert: function (message, title) {
-    if (navigator.notification) {
-        navigator.notification.alert(message, null, title, 'OK');
+  renderHomeView: function( ) {
+    var html =
+            "<div class='header'><h1>Home</h1></div>" +
+            "<div class='search-view'>" +
+            "<input class='search-key'/>" +
+            "<ul class='employee-list'></ul>" +
+            "</div>"
+    $( 'body' ).html( html );
+    $( '.search-key' ).on( 'keyup', $.proxy( this.findByName, this ) );
+  },
+
+  showAlert: function ( message, title ) {
+    if( navigator.notification ) {
+      navigator.notification.alert( message, null, title, 'OK' );
     } else {
-        alert(title ? (title + ": " + message) : message);
+      alert( title ? ( title + ": " + message ) : message );
     }
   },
 
 };
 
-app.initialize();
+app.initialize( );
