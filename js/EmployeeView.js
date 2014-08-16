5021e1022fd75646a1bfd365340e8e6f2f43ae7e
podsi <maxmilestone@gmail.com>
2014-08-16 19:05:56 +0200

    Using the Contacts API

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -4,11 +4,38 @@ var EmployeeView = function(employee) {
     this.el = $('<div/>');
 
     this.el.on('click', '.add-location-btn', this.addLocation);
+    this.el.on('click', '.add-contact-btn', this.addToContacts);
+  };
+
+  this.addToContacts = function(event) {
+    event.preventDefault();
+    console.log('addToContacts');
+
+    if (!navigator.contacts) {
+        app.showAlert("Contacts API not supported", "Error");
+        return;
+    }
+
+    var contact = navigator.contacts.create();
+
+    contact.name = {givenName: employee.firstName, familyName: employee.lastName};
+
+    var phoneNumbers = [];
+
+    phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+    phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true); // preferred number
+
+    contact.phoneNumbers = phoneNumbers;
+    contact.save();
+
+    return false;
   };
 
   this.addLocation = function(event) {
     event.preventDefault();
+
     console.log('addLocation');
+
     navigator.geolocation.getCurrentPosition(
         function(position) {
             $('.location', this.el).html(position.coords.latitude + ',' + position.coords.longitude);
@@ -16,6 +43,7 @@ var EmployeeView = function(employee) {
         function() {
             alert('Error getting location');
         });
+
     return false;
   };
 
