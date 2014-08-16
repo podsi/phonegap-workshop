0b46b66572870d67af692903445f69d0363601d2
podsi <maxmilestone@gmail.com>
2014-08-16 11:14:17 +0200

    Using native notification.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,39 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
+  findByName: function() {
+    console.log('findByName');
+    this.store.findByName($('.search-key').val(), function(employees) {
+        var l = employees.length;
+        var e;
+        $('.employee-list').empty();
+        for (var i=0; i<l; i++) {
+            e = employees[i];
+            $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+        }
+    });
+  },
 
-    initialize: function() {
-        // this.store = new MemoryStore();
-        // this.store = new LocalStorageStore();
-        this.store = new WebSqlStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+  initialize: function() {
+    var self = this;
+
+    // this.store = new MemoryStore();
+    // this.store = new LocalStorageStore();
+    // this.store = new WebSqlStore();
+
+    this.store = new WebSqlStore(function() {
+      self.showAlert('Store Initialized', 'Info');
+    });
+
+    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+  },
+
+  showAlert: function (message, title) {
+    if (navigator.notification) {
+        navigator.notification.alert(message, null, title, 'OK');
+    } else {
+        alert(title ? (title + ": " + message) : message);
     }
+  },
 
 };
 
