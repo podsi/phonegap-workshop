6bc483b21f80b1b539bb5a73b389b8909fe7efd9
podsi <maxmilestone@gmail.com>
2014-08-16 17:01:09 +0200

    Highlighting Tapped or Clicked UI Elements

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ var app = {
   initialize: function() {
     var self = this;
 
+    this.registerEvents( );
+
     this.homeTpl = Handlebars.compile( $( "#home-tpl" ).html( ) );
     this.employeeLiTpl = Handlebars.compile( $( "#employee-li-tpl" ).html( ) );
 
@@ -15,6 +17,28 @@ var app = {
     } );
 
     $( '.search-key' ).on( 'keyup', $.proxy( this.findByName, this ) );
+  },
+
+  registerEvents: function() {
+    var self = this;
+    // Check of browser supports touch events...
+    if (document.documentElement.hasOwnProperty('ontouchstart')) {
+      // ... if yes: register touch event listener to change the "selected" state of the item
+      $('body').on('touchstart', 'a', function(event) {
+          $(event.target).addClass('tappable-active');
+      });
+      $('body').on('touchend', 'a', function(event) {
+          $(event.target).removeClass('tappable-active');
+      });
+    } else {
+      // ... if not: register mouse events instead
+      $('body').on('mousedown', 'a', function(event) {
+          $(event.target).addClass('tappable-active');
+      });
+      $('body').on('mouseup', 'a', function(event) {
+          $(event.target).removeClass('tappable-active');
+      });
+    }
   },
 
   showAlert: function ( message, title ) {
